97925432ffda0b11d343ea454da3fa5d9bb8f9e1
Hromenique Batista <hromenique.batista@maplink.global>
2018-01-03 08:41:51 -0200

    após a lição 2 exercício 8

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,25 @@
 module.exports = function(app){
     app.get('/produtos', function(req, res) {
-        res.render("produtos/lista");
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'admin',
+            database: 'casadocodigo_nodejs'
+        });
+
+        connection.query('select * from produtos', function(err, results){
+            if(err){
+                console.log(err);
+                return;
+            }
+            
+            res.send(results);
+            //res.render("produtos/lista", {lista: results});
+        });
+
+        connection.end();        
+        //res.render("produtos/lista");
     });
 }
 
