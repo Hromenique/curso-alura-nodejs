9878b36d02e06f022f3641d401d2c4d1486b1ecf
Hromenique Batista <hromenique.batista@maplink.global>
2018-01-12 08:51:02 -0200

    aula 03 atividade 03 até a instalação do body-parser - atividade não finalizada

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,10 +1,10 @@
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
-        var connection = app.infra.connectionFactory();        
-        
-        var produtosBanco = new app.infra.ProdutoDAO(connection);
+        var connection = app.infra.connectionFactory();
 
-        produtosBanco.lista((erros, resultados) => {
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.lista((erros, resultados) => {
             if (erros) {
                 console.log(erros);
                 return;
@@ -16,8 +16,25 @@ module.exports = function (app) {
         connection.end();
     });
 
-    app.get('/produtos/form', function(req, res){
+    app.get('/produtos/form', function (req, res) {
         res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva', function (req, res) {
+        console.log(req);
+        
+        var produto = req.body;
+        console.log(produto);
+
+        /*
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connectionFactory);
+
+        produtosDAO.salva(produto, function (erros, resultados) {
+            res.render('produtos/lista');
+        });
+        */
+
     });
 }
 
